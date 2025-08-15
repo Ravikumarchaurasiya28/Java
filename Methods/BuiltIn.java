74a43a97734b4b2eb13791926b0978dcfe841974
Ravikumarchaurasiya28 <ravichauraiya824@gmail.com>
2025-08-15 15:54:57 +0530

    Maths Function using Builtin Mthods

---
+++ Methods/BuiltIn.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Square : " + Math.pow (n, 2));
+        System.out.println("Cube : " + Math.pow (n, 3));
+        System.out.println("Square root : " + Math.sqrt(n));
+        
+    }
+}
