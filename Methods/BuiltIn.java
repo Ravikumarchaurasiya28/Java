37f7a4c64434b758039b94af30ccdcfb9e3c5693
Ravikumarchaurasiya28 <ravichauraiya824@gmail.com>
2025-08-15 15:57:38 +0530

    Floor & ceil

--- Methods/BuiltIn.java
+++ Methods/BuiltIn.java
@@ -8,6 +8,7 @@ public class BuiltIn {
         System.out.println("Square : " + Math.pow (n, 2));
         System.out.println("Cube : " + Math.pow (n, 3));
         System.out.println("Square root : " + Math.sqrt(n));
-        
+        System.out.println("Ceil value: " + Math.ceil(1.45));
+        System.out.println("Floor value: " + Math.floor(1.45));
     }
 }
