39595a3e894f3beef1c29be5d3ecaf64f098c23a
Ravikumarchaurasiya28 <ravichauraiya824@gmail.com>
2025-08-30 11:10:45 +0530

    Palindrome String

---
+++ DSA/String/PalindromeString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s = sc.nextLine();
+        if(isPalindrome(s)) System.out.println(s + " is Palindrome String.");
+        else System.out.println(s + " is not a Palindrome String.");
+    }
+    private static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++; j--;
+        }
+        return true;
+    }
+}
