d0a4363afd9ef68ea7e618090c54eb42b5b2d812
Ravikumarchaurasiya28 <ravichauraiya824@gmail.com>
2025-09-13 16:06:55 +0530

    Exception Handling

---
+++ ExceptionHandling.java
@@ -0,0 +1,12 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int m = 5, n = 1;
+        try {
+            System.out.println(m + " Divided by " + n + " = " + (m / n));
+        }
+        catch(Exception e) {
+            System.out.println("Division is not possible due to:");
+            System.out.println(e);
+        }
+    }
+}
